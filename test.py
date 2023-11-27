8f987ac5d07bea7cc7868c62dc79d64645d288ee
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2023-11-27 17:54:09 +0900

    add

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ print('bye')
 
 
 import matplotlib as mpl
-import matplotlib.font_manager
+import matplotlib.font_manager as fm
 
 mpl.matplotlib_fname()
 
@@ -14,8 +14,4 @@ print(matplotlib.__version__) # matplotlib 버전확인
 print(matplotlib.__file__) # 설치 폴더 경로 확인
 print(matplotlib.get_cachedir()) # 캐시 폴더 경로 확인
 
-# 폰트 전체 리스트 확인
-[i.fname for i in matplotlib.font_manager.fontManager.ttflist]
-
-# 나눔 폰트 설치 확인
-[f.name for f in matplotlib.font_manager.fontManager.ttflist if 'Nanum' in f.name]
+print(mpl.rcParams['font.family'])
